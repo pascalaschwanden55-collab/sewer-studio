844deb1d1aa4b52dd1abdb52f12ea65a7c3c4252
Besitzer <besitzer@sewerstudio.local>
2026-06-21 19:52:24 +0200

    refactor: split data page column layout

---
+++ src/AuswertungPro.Next.UI/Views/Pages/DataPage.ColumnLayout.cs
@@ -0,0 +1,395 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace AuswertungPro.Next.UI.Views.Pages;
+
+public partial class DataPage
+{
+    private void Grid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        if (Grid.SelectedCells.Count > 0)
+            _activeColumn = Grid.SelectedCells[0].Column;
+        UpdateAlignmentButtonsForCurrentColumn();
+    }
+
+    private void Grid_CurrentCellChanged(object sender, EventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        if (Grid.CurrentCell.Column is not null)
+            _activeColumn = Grid.CurrentCell.Column;
+        UpdateAlignmentButtonsForCurrentColumn();
+    }
+
+    private void Grid_ColumnHeaderClick(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        if (e.OriginalSource is not DependencyObject dep)
+            return;
+
+        var header = FindAncestor<DataGridColumnHeader>(dep);
+        if (header?.Column is null)
+            return;
+
+        _activeColumn = header.Column;
+        TrySetCurrentCellForColumn(_activeColumn);
+        UpdateAlignmentButtonsForCurrentColumn();
+    }
+
+    private void Grid_ColumnReordered(object? sender, DataGridColumnEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        QueueLayoutSave();
+    }
+
+    private void AlignLeftButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyHorizontalAlignmentToCurrentColumn(HorizontalAlignment.Left);
+    }
+
+    private void AlignCenterButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyHorizontalAlignmentToCurrentColumn(HorizontalAlignment.Center);
+    }
+
+    private void AlignRightButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyHorizontalAlignmentToCurrentColumn(HorizontalAlignment.Right);
+    }
+
+    private void AlignTopButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyVerticalAlignmentToCurrentColumn(VerticalAlignment.Top);
+    }
+
+    private void AlignMiddleButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyVerticalAlignmentToCurrentColumn(VerticalAlignment.Center);
+    }
+
+    private void AlignBottomButton_Click(object sender, RoutedEventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        ApplyVerticalAlignmentToCurrentColumn(VerticalAlignment.Bottom);
+    }
+
+    private void ApplyHorizontalAlignmentToCurrentColumn(HorizontalAlignment horizontalAlignment)
+    {
+        if (_updatingAlignmentButtons)
+            return;
+
+        var column = GetActiveColumn();
+        if (column is null)
+            return;
+
+        var verticalAlignment = GetColumnVerticalAlignment(column);
+        ApplyColumnAlignment(column, horizontalAlignment, verticalAlignment);
+        UpdateAlignmentButtonsForCurrentColumn();
+    }
+
+    private void ApplyVerticalAlignmentToCurrentColumn(VerticalAlignment verticalAlignment)
+    {
+        if (_updatingAlignmentButtons)
+            return;
+
+        var column = GetActiveColumn();
+        if (column is null)
+            return;
+
+        var horizontalAlignment = GetColumnHorizontalAlignment(column);
+        ApplyColumnAlignment(column, horizontalAlignment, verticalAlignment);
+        UpdateAlignmentButtonsForCurrentColumn();
+    }
+
+    private DataGridColumn? GetActiveColumn()
+    {
+        if (_activeColumn is not null)
+            return _activeColumn;
+
+        if (Grid.CurrentCell.Column is not null)
+            return Grid.CurrentCell.Column;
+
+        if (Grid.SelectedCells.Count > 0)
+            return Grid.SelectedCells[0].Column;
+
+        return null;
+    }
+
+    private HorizontalAlignment GetColumnHorizontalAlignment(DataGridColumn column)
+    {
+        if (_columnHorizontalAlignments.TryGetValue(column, out var value))
+            return value;
+        return HorizontalAlignment.Left;
+    }
+
+    private VerticalAlignment GetColumnVerticalAlignment(DataGridColumn column)
+    {
+        if (_columnVerticalAlignments.TryGetValue(column, out var value))
+            return value;
+        return VerticalAlignment.Center;
+    }
+
+    private void TrySetCurrentCellForColumn(DataGridColumn column)
+    {
+        var rowItem = Grid.SelectedItem ?? Grid.Items.Cast<object>().FirstOrDefault();
+        if (rowItem is null)
+            return;
+
+        Grid.CurrentCell = new DataGridCellInfo(rowItem, column);
+    }
+
+    private void ApplyColumnAlignment(DataGridColumn column, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment)
+    {
+        _columnHorizontalAlignments[column] = horizontalAlignment;
+        _columnVerticalAlignments[column] = verticalAlignment;
+
+        ApplyCellAlignment(column, horizontalAlignment, verticalAlignment);
+
+        if (column is DataGridTextColumn textColumn)
+            ApplyTextColumnAlignment(textColumn, horizontalAlignment, verticalAlignment);
+
+        QueueLayoutSave();
+    }
+
+    private void ApplyCellAlignment(DataGridColumn column, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment)
+    {
+        if (!_baseCellStyles.ContainsKey(column))
+            _baseCellStyles[column] = column.CellStyle;
+
+        var style = new Style(typeof(DataGridCell), _baseCellStyles[column]);
+        style.Setters.Add(new Setter(Control.HorizontalContentAlignmentProperty, horizontalAlignment));
+        style.Setters.Add(new Setter(Control.VerticalContentAlignmentProperty, verticalAlignment));
+        column.CellStyle = style;
+    }
+
+    private void ApplyTextColumnAlignment(DataGridTextColumn column, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment)
+    {
+        if (!_baseTextElementStyles.ContainsKey(column))
+            _baseTextElementStyles[column] = column.ElementStyle;
+        if (!_baseTextEditingStyles.ContainsKey(column))
+            _baseTextEditingStyles[column] = column.EditingElementStyle;
+
+        var textAlignment = ToTextAlignment(horizontalAlignment);
+
+        var elementStyle = new Style(typeof(TextBlock), _baseTextElementStyles[column]);
+        elementStyle.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, horizontalAlignment));
+        elementStyle.Setters.Add(new Setter(TextBlock.VerticalAlignmentProperty, verticalAlignment));
+        elementStyle.Setters.Add(new Setter(TextBlock.TextAlignmentProperty, textAlignment));
+        column.ElementStyle = elementStyle;
+
+        var editingStyle = new Style(typeof(TextBox), _baseTextEditingStyles[column]);
+        editingStyle.Setters.Add(new Setter(TextBox.TextAlignmentProperty, textAlignment));
+        editingStyle.Setters.Add(new Setter(Control.HorizontalContentAlignmentProperty, horizontalAlignment));
+        editingStyle.Setters.Add(new Setter(TextBox.VerticalContentAlignmentProperty, ToTextBoxVerticalAlignment(verticalAlignment)));
+        column.EditingElementStyle = editingStyle;
+    }
+
+    private static TextAlignment ToTextAlignment(HorizontalAlignment alignment)
+    {
+        return alignment switch
+        {
+            HorizontalAlignment.Center => TextAlignment.Center,
+            HorizontalAlignment.Right => TextAlignment.Right,
+            _ => TextAlignment.Left
+        };
+    }
+
+    private static VerticalAlignment ToTextBoxVerticalAlignment(VerticalAlignment alignment)
+    {
+        return alignment switch
+        {
+            VerticalAlignment.Top => VerticalAlignment.Top,
+            VerticalAlignment.Bottom => VerticalAlignment.Bottom,
+            _ => VerticalAlignment.Center
+        };
+    }
+
+    private void UpdateAlignmentButtonsForCurrentColumn()
+    {
+        _updatingAlignmentButtons = true;
+        try
+        {
+            var column = GetActiveColumn();
+            if (column is null)
+            {
+                SetAlignmentButtonsUnchecked();
+                return;
+            }
+
+            var horizontal = GetColumnHorizontalAlignment(column);
+            var vertical = GetColumnVerticalAlignment(column);
+
+            AlignLeftButton.IsChecked = horizontal == HorizontalAlignment.Left;
+            AlignCenterButton.IsChecked = horizontal == HorizontalAlignment.Center;
+            AlignRightButton.IsChecked = horizontal == HorizontalAlignment.Right;
+
+            AlignTopButton.IsChecked = vertical == VerticalAlignment.Top;
+            AlignMiddleButton.IsChecked = vertical == VerticalAlignment.Center;
+            AlignBottomButton.IsChecked = vertical == VerticalAlignment.Bottom;
+        }
+        finally
+        {
+            _updatingAlignmentButtons = false;
+        }
+    }
+
+    private void SetAlignmentButtonsUnchecked()
+    {
+        AlignLeftButton.IsChecked = false;
+        AlignCenterButton.IsChecked = false;
+        AlignRightButton.IsChecked = false;
+        AlignTopButton.IsChecked = false;
+        AlignMiddleButton.IsChecked = false;
+        AlignBottomButton.IsChecked = false;
+    }
+
+    private void RestoreLayoutFromSettings()
+    {
+        var sp = Services;
+        var layout = sp.Settings.DataPageLayout;
+        if (layout is null)
+            return;
+
+        _isRestoringLayout = true;
+        try
+        {
+            foreach (var col in Grid.Columns)
+                AttachColumnLayoutChangeHandlers(col);
+
+            var byField = layout.Columns?
+                .Where(c => !string.IsNullOrWhiteSpace(c.FieldName))
+                .GroupBy(c => c.FieldName, StringComparer.Ordinal)
+                .ToDictionary(g => g.Key, g => g.First(), StringComparer.Ordinal)
+                ?? new Dictionary<string, DataPageColumnLayout>(StringComparer.Ordinal);
+
+            foreach (var col in Grid.Columns)
+            {
+                if (col.GetValue(FrameworkElement.TagProperty) is not string fieldName)
+                    continue;
+                if (!byField.TryGetValue(fieldName, out var state))
+                    continue;
+
+                if (state.WidthValue > 0 && Enum.TryParse<DataGridLengthUnitType>(state.WidthUnitType, out var widthType))
+                    col.Width = new DataGridLength(state.WidthValue, widthType);
+
+                var horizontal = ParseHorizontalAlignment(state.HorizontalAlignment);
+                var vertical = ParseVerticalAlignment(state.VerticalAlignment);
+                ApplyColumnAlignment(col, horizontal, vertical);
+            }
+
+            var orderedColumns = Grid.Columns
+                .Select(col =>
+                {
+                    var field = col.GetValue(FrameworkElement.TagProperty) as string;
+                    if (field is not null && byField.TryGetValue(field, out var state))
+                        return new { Column = col, Target = state.DisplayIndex, HasState = true };
+                    return new { Column = col, Target = col.DisplayIndex, HasState = false };
+                })
+                .OrderBy(x => x.HasState ? 0 : 1)
+                .ThenBy(x => x.Target)
+                .ToList();
+
+            for (var i = 0; i < orderedColumns.Count; i++)
+            {
+                try
+                {
+                    orderedColumns[i].Column.DisplayIndex = i;
+                }
+                catch
+                {
+                    // ignore invalid display index operations
+                }
+            }
+        }
+        finally
+        {
+            _isRestoringLayout = false;
+        }
+    }
+
+    private void AttachColumnLayoutChangeHandlers(DataGridColumn column)
+    {
+        DependencyPropertyDescriptor.FromProperty(DataGridColumn.WidthProperty, typeof(DataGridColumn))
+            ?.AddValueChanged(column, ColumnLayoutPropertyChanged);
+        DependencyPropertyDescriptor.FromProperty(DataGridColumn.DisplayIndexProperty, typeof(DataGridColumn))
+            ?.AddValueChanged(column, ColumnLayoutPropertyChanged);
+    }
+
+    private void ColumnLayoutPropertyChanged(object? sender, EventArgs e)
+    {
+        _ = sender;
+        _ = e;
+        QueueLayoutSave();
+    }
+
+    private void QueueLayoutSave()
+    {
+        if (_isRestoringLayout)
+            return;
+
+        _layoutSaveDebounceTimer.Stop();
+        _layoutSaveDebounceTimer.Start();
+    }
+
+    private void SaveLayoutToSettings()
+    {
+        if (_isRestoringLayout || Grid.Columns.Count == 0)
+            return;
+
+        var sp = Services;
+        var layout = sp.Settings.DataPageLayout ?? new DataPageLayoutSettings();
+        layout.Columns = Grid.Columns
+            .Select(col =>
+            {
+                var fieldName = col.GetValue(FrameworkElement.TagProperty) as string ?? "";
+                var horizontal = GetColumnHorizontalAlignment(col).ToString();
+                var vertical = GetColumnVerticalAlignment(col).ToString();
+                return new DataPageColumnLayout
+                {
+                    FieldName = fieldName,
+                    DisplayIndex = col.DisplayIndex,
+                    WidthValue = col.Width.Value,
+                    WidthUnitType = col.Width.UnitType.ToString(),
+                    HorizontalAlignment = horizontal,
+                    VerticalAlignment = vertical
+                };
+            })
+            .Where(x => !string.IsNullOrWhiteSpace(x.FieldName))
+            .ToList();
+        sp.Settings.DataPageLayout = layout;
+        sp.Settings.Save();
+    }
+
+    private static HorizontalAlignment ParseHorizontalAlignment(string? value)
+    {
+        if (Enum.TryParse<HorizontalAlignment>(value, out var parsed))
+            return parsed;
+        return HorizontalAlignment.Left;
+    }
+
+    private static VerticalAlignment ParseVerticalAlignment(string? value)
+    {
+        if (Enum.TryParse<VerticalAlignment>(value, out var parsed))
+            return parsed;
+        return VerticalAlignment.Center;
+    }
+}
