e6eeeffb177847e63affdbe709f09e610e1eb1a1
Besitzer <besitzer@sewerstudio.local>
2026-06-29 21:23:20 +0200

    refactor: teile DataGrid-Kostenspalte

--- src/AuswertungPro.Next.UI/Views/Pages/SchaechtePage.xaml.cs
+++ src/AuswertungPro.Next.UI/Views/Pages/SchaechtePage.xaml.cs
@@ -37,7 +37,6 @@ public partial class SchaechtePage : UserControl
         public string OptionField { get; }
     }
 
-    private static readonly IValueConverter CostDisplayConverter = new ChfAccountingDisplayConverter();
     private static readonly IValueConverter HorizontalAlignmentToTextAlignmentConverter = new DataGridHorizontalAlignmentToTextAlignmentConverter();
     private static readonly Regex NonNumericRegex = new("[^0-9]", RegexOptions.Compiled);
 
@@ -120,7 +119,7 @@ public partial class SchaechtePage : UserControl
                 DataGridColumn column;
                 if (IsCostColumn(col))
                 {
-                    column = CreateCostColumn(col);
+                    column = DataGridCostColumnFactory.Create(col, col);
                 }
                 else if (IsZustandsklasseColumn(col))
                 {
@@ -386,81 +385,6 @@ public partial class SchaechtePage : UserControl
             Header = recordField,
             CellTemplate = new DataTemplate { VisualTree = displayFactory },
             CellEditingTemplate = new DataTemplate { VisualTree = comboFactory },
-            Width = DataGridLength.SizeToHeader
-        };
-    }
-
-    private DataGridTemplateColumn CreateCostColumn(string recordField)
-    {
-        var displayPanel = new FrameworkElementFactory(typeof(DockPanel));
-        displayPanel.SetValue(DockPanel.LastChildFillProperty, true);
-        displayPanel.SetValue(FrameworkElement.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
-        displayPanel.SetBinding(FrameworkElement.WidthProperty, new Binding("ActualWidth")
-        {
-            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(DataGridCell), 1)
-        });
-
-        var displayCurrency = new FrameworkElementFactory(typeof(TextBlock));
-        displayCurrency.SetValue(DockPanel.DockProperty, Dock.Left);
-        displayCurrency.SetValue(FrameworkElement.MarginProperty, new Thickness(0, 0, 8, 0));
-        displayCurrency.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
-        displayCurrency.SetBinding(TextBlock.TextProperty, new Binding($"Fields[{recordField}]")
-        {
-            Converter = CostDisplayConverter,
-            ConverterParameter = "currency"
-        });
-        displayPanel.AppendChild(displayCurrency);
-
-        var displayAmount = new FrameworkElementFactory(typeof(TextBlock));
-        displayAmount.SetValue(FrameworkElement.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
-        displayAmount.SetValue(TextBlock.TextAlignmentProperty, TextAlignment.Right);
-        displayAmount.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
-        displayAmount.SetValue(TextBlock.TextTrimmingProperty, TextTrimming.CharacterEllipsis);
-        displayAmount.SetBinding(TextBlock.TextProperty, new Binding($"Fields[{recordField}]")
-        {
-            Converter = CostDisplayConverter,
-            ConverterParameter = "amount"
-        });
-        displayPanel.AppendChild(displayAmount);
-
-        var editPanel = new FrameworkElementFactory(typeof(DockPanel));
-        editPanel.SetValue(DockPanel.LastChildFillProperty, true);
-        editPanel.SetValue(FrameworkElement.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
-        editPanel.SetBinding(FrameworkElement.WidthProperty, new Binding("ActualWidth")
-        {
-            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(DataGridCell), 1)
-        });
-
-        var editCurrency = new FrameworkElementFactory(typeof(TextBlock));
-        editCurrency.SetValue(DockPanel.DockProperty, Dock.Left);
-        editCurrency.SetValue(FrameworkElement.MarginProperty, new Thickness(0, 0, 8, 0));
-        editCurrency.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
-        editCurrency.SetBinding(TextBlock.TextProperty, new Binding($"Fields[{recordField}]")
-        {
-            Converter = CostDisplayConverter,
-            ConverterParameter = "currency"
-        });
-        editPanel.AppendChild(editCurrency);
-
-        var editAmount = new FrameworkElementFactory(typeof(TextBox));
-        editAmount.SetValue(FrameworkElement.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
-        editAmount.SetValue(TextBox.TextAlignmentProperty, TextAlignment.Right);
-        editAmount.SetValue(Control.VerticalContentAlignmentProperty, VerticalAlignment.Center);
-        editAmount.SetBinding(TextBox.TextProperty, new Binding($"Fields[{recordField}]")
-        {
-            Mode = BindingMode.TwoWay,
-            UpdateSourceTrigger = UpdateSourceTrigger.LostFocus,
-            Converter = CostDisplayConverter,
-            ConverterParameter = "amount"
-        });
-        editPanel.AppendChild(editAmount);
-
-        return new DataGridTemplateColumn
-        {
-            Header = recordField,
-            CellTemplate = new DataTemplate { VisualTree = displayPanel },
-            CellEditingTemplate = new DataTemplate { VisualTree = editPanel },
-            SortMemberPath = $"Fields[{recordField}]",
             Width = DataGridLength.SizeToHeader
         };
     }
